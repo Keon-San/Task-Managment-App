598a13b95e5ca949ae8c8b69a63dd6e6d5ba6cb5
unknown <keonsanavandijoon@gmail.com>
2019-12-05 10:49:57 -0500

    Made database structure, starting on template

--- src/taskmanager/admin.py
+++ src/taskmanager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+import taskmanager.models as db
 
 # Register your models here.
+admin.site.register(db.Role)
+admin.site.register(db.User)
+admin.site.register(db.Task)
+admin.site.register(db.List)
+admin.site.register(db.Note)
--- src/taskmanager/models.py
+++ src/taskmanager/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    role_name = models.CharField(max_length=100, primary_key=True)
+    controllingRole = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.role_name
+
+class User(models.Model):
+    email = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
+    roles = models.ManyToManyField(Role)
+
+    def __str__(self):
+        return self.name + ": " + self.email
+
+class List(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    can_read = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, related_name='viewable_lists')
+    can_claim = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, related_name='claimable_lists')
+    can_add = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, related_name='addable_lists')
+    
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_tasks')
+    claimants = models.ManyToManyField(User, related_name='claimed_tasks')
+    title = models.CharField(max_length=10000)
+    task_list = models.ForeignKey(List, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000000000)
+    priority = models.IntegerField()
+    larger_goal = models.CharField(max_length=10000, null=True)
+    estimate_end_date = models.DateField()
+    #WIP OTHER DATA
+
+    def __str__(self):
+        return self.title
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000000000000)
+    poster = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.title
